9719990edb08e584317fd799cea08d7508edf834
simba <simbadevel@gmail.com>
2015-06-07 08:48:26 -0700

    Refactored generic parts of dining into scraper
    Some parts of dining.go were generic, such as the Path method. These generic pieces were refactored down into scraper.go to be used in other scraper packages in the future

--- cloud/scraper/dining/dining.go
+++ cloud/scraper/dining/dining.go
@@ -2,73 +2,54 @@ package dining
 
 import (
 	"fmt"
-	"github.com/PuerkitoBio/goquery"
+	"github.com/ucscstudentapp/cloud/scraper"
 	"os"
 )
 
-type PathNode struct {
-	Name string
-	Idx  int
-}
 
 const (
-	UNIQ    = -1
-	ALL     = -2
 	WEBSITE = "http://nutrition.sa.ucsc.edu/menuSamp.asp?locationNum=30&locationName=&sName=&naFlag="
 )
 
 var (
-	MENU_TABLE_PATH = []PathNode{
-		{"html", UNIQ},
-		{"body", UNIQ},
+	MENU_TABLE_PATH = []scraper.Node{
+		{"html", scraper.UNIQ},
+		{"body", scraper.UNIQ},
 		{"table", 0},
-		{"tbody", UNIQ},
-		{"tr", UNIQ},
+		{"tbody", scraper.UNIQ},
+		{"tr", scraper.UNIQ},
+		{"td", scraper.ALL},
 	}
-	MENU_ROWS_PATH = []PathNode{
-		{"table", UNIQ},
-		{"tbody", UNIQ},
+	MENU_ROWS_PATH = []scraper.Node{
+		{"table", scraper.UNIQ},
+		{"tbody", scraper.UNIQ},
 		{"tr", 1},
-		{"td", UNIQ},
-		{"table", UNIQ},
-		{"tbody", UNIQ},
+		{"td", scraper.UNIQ},
+		{"table", scraper.UNIQ},
+		{"tbody", scraper.UNIQ},
+		{"tr", scraper.UNIQ},
 	}
-	ROW_NAME_PATH = []PathNode{
+	ROW_NAME_PATH = []scraper.Node{
 		{"td", 0},
-		{"table", UNIQ},
-		{"tbody", UNIQ},
-		{"tr", UNIQ},
-		{"td", UNIQ},
-		{"div.menusamprecipes", UNIQ},
-		{"span", UNIQ},
+		{"table", scraper.UNIQ},
+		{"tbody", scraper.UNIQ},
+		{"tr", scraper.UNIQ},
+		{"td", scraper.UNIQ},
+		{"div.menusamprecipes", scraper.UNIQ},
+		{"span", scraper.UNIQ},
 	}
 )
 
-type selection struct {
-	*goquery.Selection
+type menuTable struct {
+	scraper.Selection
 }
 
-type menuTable selection
-type menuDoc selection
+type menuDoc struct {
+	scraper.Selection
+}
 
 type MenuItem struct {
 	name string
-}
-
-func (sel selection) Print() {
-	for i, v := range sel.Nodes {
-		fmt.Printf("%d: %s\n", i, v.Data)
-	}
-}
-
-func (sel selection) PrintChildren() {
-	selection{sel.Children()}.Print()
-}
-
-func (sel selection) PrintNodes() {
-	for i, v := range sel.Contents().Nodes {
-		fmt.Printf("%d: %s\n", i, v.Data)
-	}
 }
 
 func failOnError(err error) {
@@ -79,20 +60,19 @@ func failOnError(err error) {
 }
 
 func (table menuTable) parseMenuItems(idx int) []MenuItem {
-	selection{table.Children().Filter("td").Eq(idx).Children().Filter("table").Children().Filter("tbody").Children().Filter("tr").Eq(1).Children().Filter("td").Children().Filter("table").Children().Filter("tbody")}.PrintChildren()
-	rows := selection{selection{table.Children().Filter("td").Eq(idx)}.transPath(MENU_ROWS_PATH).Children().Filter("tr")}
+	rows := table.Index(idx).Path(MENU_ROWS_PATH)
 	size := rows.Size()
 	items := make([]MenuItem, size)
-	fmt.Println(size)
 	for i := 1; i < size; i++ {
-		selection{rows.Eq(i)}.transPath(ROW_NAME_PATH).PrintNodes()
-		items[i-1] = MenuItem{selection{rows.Eq(i)}.transPath(ROW_NAME_PATH).Contents().Get(0).Data}
+		rows.Index(i).Path(ROW_NAME_PATH).Print()
+		menuNameNode := rows.Index(i).Path(ROW_NAME_PATH)
+		items[i-1] = MenuItem{menuNameNode.Inner(0).Data}
 	}
 	return items
 }
 
 func (table menuTable) parseBreakfastMenu() []MenuItem {
-	if table.Children().Size() != 3 {
+	if table.Size() != 3 {
 		return nil
 	} else {
 		return table.parseMenuItems(0)
@@ -100,40 +80,34 @@ func (table menuTable) parseBreakfastMenu() []MenuItem {
 }
 
 func (table menuTable) parseLunchMenu() []MenuItem {
-	if table.Children().Size() != 3 {
+	if size := table.Size(); size == 1 {
+		return nil
+	} else if size == 2 {
 		return table.parseMenuItems(0)
+
 	} else {
 		return table.parseMenuItems(1)
 	}
 }
 
 func (table menuTable) parseDinnerMenu() []MenuItem {
-	if table.Children().Size() != 3 {
+	if size := table.Size(); size == 1 {
+		return nil
+	} else if size == 2 {
 		return table.parseMenuItems(1)
 	} else {
 		return table.parseMenuItems(2)
 	}
 }
 
-func (sel selection) transPath(nodes []PathNode) (final selection) {
-	final = sel
-	for _, v := range nodes {
-		children := selection{final.Children()}
-		final = selection{children.Filter(v.Name)}
-		if idx := v.Idx; idx != UNIQ {
-			final = selection{final.Eq(idx)}
-		}
-	}
-	return
-}
 
 func (doc menuDoc) selectMenuTable() menuTable {
-	selection := selection(doc).transPath(MENU_TABLE_PATH)
-	return menuTable(selection)
+	sel := doc.Path(MENU_TABLE_PATH)
+	return menuTable{sel}
 }
 
 func c8Doc() menuDoc {
-	doc, err := goquery.NewDocument(WEBSITE)
+	menu, err := scraper.New(WEBSITE)
 	failOnError(err)
-	return menuDoc{(*doc).Selection}
+	return menuDoc{menu}
 }
--- cloud/scraper/scraper.go
+++ cloud/scraper/scraper.go
@@ -1,1 +1,69 @@
 package scraper
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"golang.org/x/net/html"
+)
+
+type Node struct {
+	Name string
+	Idx  int
+}
+
+const (
+	UNIQ    = -1
+	ALL     = -2
+)
+
+type Selection struct {
+	impl *goquery.Selection
+}
+
+func (sel Selection) Print() {
+	for i, v := range sel.impl.Nodes {
+		fmt.Printf("%d: %s\n", i, v.Data)
+	}
+}
+
+func (sel Selection) PrintChildren() {
+	children := Selection{sel.impl.Children()}
+	children.Print()
+}
+
+
+func New(url string) (Selection, error) {
+	doc, err := goquery.NewDocument(url)
+	return Selection{(*doc).Selection}, err
+}
+
+func (sel Selection) ChildrenFilter(selector string) Selection {
+	return Selection{sel.impl.Children().Filter(selector)}
+}
+
+func (sel Selection) Index(idx int) Selection {
+	return Selection{sel.impl.Eq(idx)}
+}
+
+func (sel Selection) Children() Selection {
+	return Selection{sel.impl.Children()}
+}
+
+func (sel Selection) Size() int {
+	return sel.impl.Size()
+}
+
+func (sel Selection) Inner(idx int) *html.Node {
+	return sel.impl.Contents().Get(idx)
+}
+
+func (sel Selection) Path(nodes []Node) Selection {
+	final := sel
+	for _, v := range nodes {
+		final = final.ChildrenFilter(v.Name)
+		if idx := v.Idx; idx != UNIQ && idx != ALL{
+			final = final.Index(idx)
+		}
+	}
+	return final
+}
