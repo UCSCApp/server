a29b0fddc7eb96e0fe6b5b8c30890d20f77a7093
simba <simbadevel@gmail.com>
2015-06-07 08:46:55 -0700

    Added dining hall scraper test for all menus
    Dining hall scraper test now prints out breakfast, lunch, and dinner menu

--- cloud/scraper/dining/dining_test.go
+++ cloud/scraper/dining/dining_test.go
@@ -6,5 +6,7 @@ import (
 )
 
 func TestSelectC8(t *testing.T) {
+	fmt.Printf("%#v", c8Doc().selectMenuTable().parseBreakfastMenu())
+	fmt.Printf("%#v", c8Doc().selectMenuTable().parseLunchMenu())
 	fmt.Printf("%#v", c8Doc().selectMenuTable().parseDinnerMenu())
 }
